44316c965c6d63575cc7bd183d21e1d3ee3dd568
abdel-mars <elmahmoudiabderahman@gmail.com>
2025-01-13 18:13:18 +0100

    added func

--- main.go
+++ main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main () {
 
+	if len(os.Args) == 3 {
+		str := ReadFileString(os.Args[1])
+		startArrBrac := indexOfStartBrac(str)
+		endArrBrac := indexOfEndBrac(str)
+		lenStartArrBrac := len(startArrBrac)
+		
+
+	}
+	
+
+}
+
+func indexOfEndBrac(s string) []int {
+	var res []int
+	for i, x := range s {
+		if x == ')' {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+func ReadFileString(s string) string {
+	b, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Print(err)
+	}
+	return string(b)
+}
+
+func indexOfStartBrac(s string) []int {
+	var res []int 
+	for i, x := range s {
+		if x == '(' {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+func Low(S string) string {
+	words := string.Fields(s)
+	for i := 0; i < len(words); i++ {
+		if strings.HasPrefix(words[i], "(low)") 
+
+	}
 }
--- utils.go
+++
@@ -1,5 +0,0 @@
-package piscine
-
-import "piscine"
-
-kl
